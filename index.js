51a829cef70c4fdfaa2d1c71a11aab8b3a1f0d77
odioraivaodio <rms.mariana1@gmail.com>
2023-11-23 07:34:24 -0300

    Recebendo parâmetro na rota

--- index.js
+++ index.js
@@ -5,32 +5,29 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
-app.use(express.urlencoded ({
-    extended: true
-}))
-
  // GET E POST
+
 app.post('/cadastrar/salvar', (requisicao, resposta) => {
     console.log(requisicao.body)
 })
 
 app.get('/cadastrar', (requisicao, resposta) => {
-    resposta.sendFile('${caminhoBase}/cadastro.html')
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
 })
 
  // https://localhost:3000/usuarios/
-app.get('/usuarios/:identificador', (requisicao, resposta) => {
+app.get('/usuarios/:id', (requisicao, resposta) => {
 
-    const id = require.params.id
+    const id = requisicao.params.id
     
-    console.log('Acessando dados do usuário ${identificador}')
+    console.log(`Acessando dados do usuário ${id}`)
 
-    resposta.sendFile('${caminhoBase}/usuarios.html')
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
 })
 
  // https://localhost:3000
 app.get ('/', (requisicao, resposta) => {
-    resposta.sendFile('${caminhoBase}/index.html')
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
